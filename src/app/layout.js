b7750c257d670c1c85f9e05ee9821ca69d2ca40b
raulrazo <raulrazox@gmail.com>
2024-07-19 21:47:50 -0700

    navbar exists and fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Landing from "@/components/Landing";
+import Navigation from "@/components/Navigation";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +13,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+		<html lang="en">
+			<body className={inter.className}>
+				<Navigation />
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
 }
